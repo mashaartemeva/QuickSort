1ff8d3cc4c87219164f39421c98400e3056b1c3b
mashaartemeva <masha94154@gmail.com>
2019-03-17 23:22:39 +0500

    .

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    public class ClassSort
+    {
+        public static void QuickSort(int[] array)
+        {
+            if (array is null) return;
+            QuickSort(array, 0, array.Length - 1);
+        }
+        public static void QuickSort(int[] array, int first, int last)
+        {
+            int p = array[(last - first) / 2 + first];
+            int temp;
+            int i = first, j = last;
+            while (i <= j)
+            {
+                while (array[i] < p && i <= last) ++i;
+                while (array[j] > p && j >= first) --j;
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    ++i; --j;
+                }
+            }
+            if (j > first) QuickSort(array, first, j);
+            if (i < last) QuickSort(array, i, last);
+        }
+        static void Main()
+        {
+
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 5);
+                Console.WriteLine(array[i]);
+            }
+            QuickSort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+        static Random random = new Random();
+
+    }
+}
---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sort
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSort3Element()
+        {
+            var array = new int[3] { 3, 1, 2 };
+            ClassSort.QuickSort(array);
+            Assert.IsTrue(array[1] >= array[0]);
+            Assert.IsTrue(array[2] >= array[1]);
+        }
+        [TestMethod]
+        public void TestSort100SameElement()
+        {
+            var array = new int[100];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++) array[i] = 11;
+            ClassSort.QuickSort(array);
+            Assert.IsTrue(array[rand.Next(0,100)] == array[rand.Next(0, 100)]);
+            
+        }
+        [TestMethod]
+        public void TestSort1000RandomElement()
+        {
+            var array = new int[1000];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++) array[i] = rand.Next(0,100);
+            ClassSort.QuickSort(array);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.IsTrue(array[rand.Next(0, 500)] <= array[rand.Next(500, 1000)]);
+            }
+        }
+        [TestMethod]
+        public void TestSortEmptyArray()
+        {
+            var array = new int[5];
+            ClassSort.QuickSort(array);
+        }
+
+        [TestMethod]
+        public void VeryDifficultForMyCalculator()
+        {
+            var array = new int[1500000000];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++) array[i] = rand.Next(0, 100);
+            ClassSort.QuickSort(array);
+            Assert.IsTrue(array[rand.Next(0, 750000000)] <= array[rand.Next(750000000, 1500000000)]);
+        }
+    }
+}
